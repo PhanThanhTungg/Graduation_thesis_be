1c252988bd68783d8464e658412c12adff25d8e9
hcthinhjr03 <thinhhacuong123@gmail.com>
2025-11-10 11:20:46 +0700

    impl(admin): soft delete course endpoint

--- src/common/interfaces/objectFilter.interface.ts
+++ src/common/interfaces/objectFilter.interface.ts
@@ -14,5 +14,6 @@ export interface sortFilter {
 
 export interface fullObjectFilter extends objectSearchFilter, paginationFilter, sortFilter {
   isPublished?: string | boolean;
+  includeDeleted?: string | boolean;
 }
 export interface categoryFilter extends objectSearchFilter, sortFilter {}
